e0795b127a79100974225f16e08e75c45876387e
conconrob <connortprobertson@gmail.com>
2019-02-01 09:54:02 +0000

    ADDED sliding to carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,27 +2,31 @@ class Carousel {
   constructor(element) {
     this.element = element;
     this.currentImg = 0;
+    this.slideTime = 0.2;
     // get the left and right button
     this.buttonLeft = this.element.querySelector('.left-button');
     this.buttonRight = this.element.querySelector('.right-button');
     // get a list of the images
     this.images = this.element.querySelectorAll('img');
+    // display init image
+    // this.displayImg();
+    this.slideInLeft();
     // add event listeners to the buttons
     this.buttonLeft.addEventListener('click', () => {
-      this.hideImg();
+      this.slideOutLeft();
       this.currentImg--;
       if (this.currentImg === -1) {
         this.currentImg = this.images.length - 1;
       }
-      this.displayImg();
+      setTimeout(() => { this.slideInLeft(); }, this.slideTime * 1000);
     });
     this.buttonRight.addEventListener('click', () => {
-      this.hideImg();
+      this.slideOutRight();
       this.currentImg++;
       if (this.currentImg === this.images.length) {
         this.currentImg = 0;
       }
-      this.displayImg();
+      setTimeout(() => { this.slideInRight(); }, this.slideTime * 1000);
     });
   }
 
@@ -33,8 +37,25 @@ class Carousel {
   hideImg() {
     this.images[this.currentImg].style.display = 'none';
   }
-}
 
+  slideInLeft() {
+    TweenMax.fromTo(this.images[this.currentImg], this.slideTime, { x: 100, opacity: 0, display: 'none' }, { x: 0, opacity: 1, display: 'block' });
+  }
+
+  slideOutLeft() {
+    TweenMax.to(this.images[this.currentImg], this.slideTime, { x: -100, opacity: 0, display: 'none' });
+  }
+
+  slideInRight() {
+    TweenMax.fromTo(this.images[this.currentImg], this.slideTime, { x: -100, opacity: 0, display: 'none' }, { x: 0, opacity: 1, display: 'block' });
+  }
+
+  slideOutRight() {
+    TweenMax.to(this.images[this.currentImg], this.slideTime, { x: 100, opacity: 0, display: 'none' });
+  }
+}
+// TweenMax.fromTo(element, 1, {css: {left: "100px", top: "100px"}}, {css:{left:"130px", top: "130px"}} )
+// TweenMax.to(this.element, 0.2, { opacity: 1, display: 'block' });
 let carousel = document.querySelector('.carousel');
 carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
