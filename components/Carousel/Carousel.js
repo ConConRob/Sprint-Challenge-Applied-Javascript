d50c1787c6642cc58e593d8369e1609c949a9feb
conconrob <connortprobertson@gmail.com>
2019-02-01 09:12:36 +0000

    DONE variables of carousel class

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,21 @@
 class Carousel {
-
+  constructor(element) {
+    this.element = element;
+    this.currentImg
+    // get the left and right button
+    this.buttonLeft = this.element.querySelector('.left-button');
+    this.buttonRight = this.element.querySelector('.right-button');
+    // get a list of the images
+    this.images = this.element.querySelectorAll('img')
+    console.log(this.images);
+    // add event listeners to the buttons
+    //this.buttonLeft.addEventListener('click', () =>)
+    console.log(this.buttonRight);
+  }
 }
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
